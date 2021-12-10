dd8bb8a37f65977261fb3ab5e908003e458ff821
javdev20 <islam.qayumov07@gmail.com>
2021-12-10 11:11:42 +0300

    add menuitems with addMenuItem() method

--- src/gui/menu/MainMenu.java
+++ src/gui/menu/MainMenu.java
@@ -6,7 +6,9 @@ import settings.Text;
 import javax.swing.*;
 
 public class MainMenu extends JMenuBar {
-    
+
+    private JMenuItem menuEdit;
+    private JMenuItem menuDelete;
     private final MainFrame frame;
     
     public MainMenu(MainFrame frame) {
@@ -21,11 +23,53 @@ public class MainMenu extends JMenuBar {
         JMenu view = new JMenu(Text.get("MENU_VIEW"));
         JMenu settings = new JMenu(Text.get("MENU_SETTINGS"));
         JMenu help = new JMenu(Text.get("MENU_HELP"));
+        JMenu language = new JMenu(Text.get("MENU_SETTINGS_LANGUAGE"));
 
         add(file);
         add(edit);
         add(view);
         add(settings);
         add(help);
+
+        addMenuItem(file, Text.get("MENU_FILE_NEW"));
+        addMenuItem(file, Text.get("MENU_FILE_OPEN"));
+        addMenuItem(file, Text.get("MENU_FILE_SAVE"));
+        addMenuItem(file, Text.get("MENU_FILE_UPDATE_CURRENCIES"));
+        addMenuItem(file, Text.get("MENU_FILE_EXIT"));
+
+        addMenuItem(edit, Text.get("MENU_EDIT_ADD"));
+        menuEdit = addMenuItem(edit, Text.get("MENU_EDIT_EDIT"));
+        menuDelete = addMenuItem(edit, Text.get("MENU_EDIT_DELETE"));
+        menuEdit.setEnabled(false);
+        menuDelete.setEnabled(false);
+
+        addMenuItem(view, Text.get("MENU_VIEW_OVERVIEW"));
+        addMenuItem(view, Text.get("MENU_VIEW_ACCOUNTS"));
+        addMenuItem(view, Text.get("MENU_VIEW_ARTICLES"));
+        addMenuItem(view, Text.get("MENU_VIEW_TRANSACTIONS"));
+        addMenuItem(view, Text.get("MENU_VIEW_TRANSFERS"));
+        addMenuItem(view, Text.get("MENU_VIEW_CURRENCIES"));
+        addMenuItem(view, Text.get("MENU_VIEW_STATISTICS"));
+
+        addMenuItem(help, Text.get("MENU_HELP_ABOUT"));
+
+
+        ButtonGroup group = new ButtonGroup();
+        JRadioButtonMenuItem menuRussian = new JRadioButtonMenuItem(Text.get("MENU_SETTINGS_LANGUAGE_RUSSIAN"));
+        JRadioButtonMenuItem menuEnglish = new JRadioButtonMenuItem(Text.get("MENU_SETTINGS_LANGUAGE_ENGLISH"));
+        group.add(menuRussian);
+        group.add(menuEnglish);
+
+        language.add(menuRussian);
+        language.add(menuEnglish);
+
+        settings.add(language);
     }
+
+    private JMenuItem addMenuItem(JMenu jMenu, String title) {
+        JMenuItem menuItem = new JMenuItem(title);
+        jMenu.add(menuItem);
+        return menuItem;
+    }
+
 }
--- src/settings/Text.java
+++ src/settings/Text.java
@@ -26,5 +26,23 @@ final public class Text {
         data.put("MENU_FILE_SAVE", "Сохранить");
         data.put("MENU_FILE_UPDATE_CURRENCIES", "Обновить курс валют");
         data.put("MENU_FILE_EXIT", "Выход");
+
+        data.put("MENU_EDIT_ADD", "Добавить");
+        data.put("MENU_EDIT_EDIT", "Изменить");
+        data.put("MENU_EDIT_DELETE", "Удалить");
+
+        data.put("MENU_VIEW_OVERVIEW", "Обзор");
+        data.put("MENU_VIEW_ACCOUNTS", "Счета");
+        data.put("MENU_VIEW_ARTICLES", "Статьи");
+        data.put("MENU_VIEW_TRANSACTIONS", "Транзакции");
+        data.put("MENU_VIEW_TRANSFERS", "Переводы");
+        data.put("MENU_VIEW_CURRENCIES", "Валюты");
+        data.put("MENU_VIEW_STATISTICS", "Статистика");
+
+        data.put("MENU_SETTINGS_LANGUAGE", "Язык");
+        data.put("MENU_SETTINGS_LANGUAGE_RUSSIAN", "Русский");
+        data.put("MENU_SETTINGS_LANGUAGE_ENGLISH", "Английский");
+
+        data.put("MENU_HELP_ABOUT", "О программе");
     }
 }
