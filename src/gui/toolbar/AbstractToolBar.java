85984fb213e27b4b8510c42a46146003a21c6ad2
javdev20 <islam.qayumov07@gmail.com>
2021-12-17 20:30:20 +0300

    Some changes in MainToolBar and AbstractToolBar

--- src/gui/toolbar/AbstractToolBar.java
+++ src/gui/toolbar/AbstractToolBar.java
@@ -1,6 +1,8 @@
 package gui.toolbar;
 
 import gui.MainButton;
+import gui.Refresh;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
@@ -8,19 +10,32 @@ import javax.swing.border.EmptyBorder;
  *
  * @author Admin
  */
-abstract public class AbstractToolBar extends JPanel {
+abstract public class AbstractToolBar extends JPanel implements Refresh {
 
-    public AbstractToolBar(EmptyBorder border) {
-        super();
-        setBorder(border);
+    public AbstractToolBar() {
+
     }
     
     abstract protected void init();
-    
-    protected MainButton addButton(String title, ImageIcon icon) {
-        MainButton button = new MainButton(title, icon);
+
+    protected MainButton addButton(String title, ImageIcon icon, String action, boolean topIcon) {
+        MainButton button = new MainButton(title, icon, null, action);
+        if (topIcon) {
+            button.setHorizontalTextPosition(SwingConstants.CENTER);
+            button.setVerticalTextPosition(SwingConstants.BOTTOM);
+        }
+        else {
+            button.setHorizontalTextPosition(SwingConstants.RIGHT);
+            button.setVerticalTextPosition(SwingConstants.CENTER);
+        }
         add(button);
         return button;
+    }
+
+    @Override
+    public void refresh() {
+        removeAll();
+        init();
     }
 
 }
